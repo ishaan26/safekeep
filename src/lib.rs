7cc6e167fb3960d085cc0ed2803b5eee35f9fa7a
Ishaan Goel <ishaangoel.99@gmail.com>
2026-01-27 18:08:00 +0530

    chore(api): thinking about a proper api.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,15 @@
 mod error;
 
 use error::BackupError;
-use std::path::Path;
+use std::path::{Path, PathBuf};
+
+pub struct BackupOptions {
+    path: PathBuf,
+    backup_data: Vec<Box<dyn BackupType>>,
+    no_index: bool,
+    with_compression: bool,
+    with_encryption: bool,
+}
 
 /// Only the types that implement this trait can be backed-up.
 pub trait BackupType {
